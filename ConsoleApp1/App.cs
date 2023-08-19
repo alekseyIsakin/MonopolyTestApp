65618b6642f103097b896ed139d7cf0aceaabeb2
karahunforever <karahunforever@gmail.com>
2023-08-19 19:20:03 +0300

    Move class to another file

--- ConsoleApp1/App.cs
+++ ConsoleApp1/App.cs
@@ -1,156 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal interface Cargo
-    {
-        public Guid id { get; }
-        public int width { get; }
-        public int length { get; }
-        public int heigth { get; }
-        public int weight { get; }
-        public int volume { get; }
-    }
-
-    internal class Box : Cargo
-    {
-        public Box(
-            Guid id,
-            int width, int length, int heigth,
-            int weight,
-            DateOnly? manufactDate,
-            DateOnly? expirationDate)
-        {
-            this.id = id;
-            this.width = width;
-            this.length = length;
-            this.heigth = heigth;
-            this.weight = weight;
-            this._manufactDate = manufactDate;
-            this._expirationDate = expirationDate;
-        }
-
-        public Box(
-            int width, int length, int heigth,
-            int weight,
-            DateOnly? manufactDate,
-            DateOnly? expirationDate)
-            : this(Guid.NewGuid(), width, length, heigth, weight, manufactDate, expirationDate)
-        { }
-        public Box() { }
-
-        private DateOnly? _manufactDate;
-        private DateOnly? _expirationDate;
-
-        public void SetExpirationDate(int year, int month, int day)
-            => _expirationDate = new DateOnly(year, month, day);
-
-        public void SetManufactionDate(int year, int month, int day)
-            => _manufactDate = new DateOnly(year, month, day);
-
-
-        public DateOnly ExpirationDate
-        {
-            get => _expirationDate ??
-                   _manufactDate?.AddDays(100) ??
-                   DateOnly.MinValue;
-        }
-
-        public Guid id { get; }
-        public int width { get; }
-        public int length { get; }
-        public int heigth { get; }
-        public int weight { get; }
-        public int volume { get => width * length * heigth; }
-    }
-
-    internal class Pallet : Cargo
-    {
-        public Pallet(Guid id, int width, int length, int heigth)
-        {
-            this.id = id;
-            this.width = width;
-            this.length = length;
-            this.P_heigth = heigth;
-
-            _boxes = new List<Box>();
-        }
-
-        public Pallet(int width, int length, int heigth)
-            : this(Guid.NewGuid(), width, length, heigth) { }
-
-        private List<Box> _boxes { get; set; }
-        public int P_heigth { get; }
-        public int P_volume
-        {
-            get => width * length * P_heigth;
-        }
-        public int Boxes_height
-        {
-            get => _boxes.Aggregate(0, (heigth, box) => heigth + box.heigth);
-        }
-        public int Boxes_weight
-        {
-            get => _boxes.Aggregate(0, (weight, box) => weight + box.weight);
-        }
-        public int Boxes_volume
-        {
-            get => _boxes.Aggregate(0, (volume, box) => volume + box.volume);
-        }
-        const int P_weight = 30;
-
-        public DateOnly ExpirationDate
-        {
-            get => _boxes.Count > 0 ?
-                _boxes.Min(box => box.ExpirationDate) :
-                DateOnly.MinValue;
-        }
-        public List<DateOnly> ExpirationDates
-        {
-            get => _boxes
-                .Select(b => b.ExpirationDate)
-                .ToList();
-        }
-
-        public Guid id { get; }
-        public int width { get; }
-        public int length { get; }
-        public int heigth
-        {
-            get => P_heigth + Boxes_height;
-        }
-        public int weight
-        {
-            get => P_weight + Boxes_weight;
-        }
-        public int volume
-        {
-            get => P_volume + Boxes_volume;
-        }
-
-        public List<Box> GetBoxes()
-        {
-            return _boxes;
-        }
-        public bool AddBox(Box box)
-        {
-            var can_hold = (box.width <= width && box.length <= length) ||
-                           (box.width <= length && box.width <= length);
-
-            if (can_hold)
-            {
-                _boxes.Add(box);
-            }
-
-            return can_hold;
-        }
-    }
     public class App
     {
         public static void Main()
@@ -198,42 +54,32 @@ namespace ConsoleApp1
                     p.AddBox(b);
                 }
                 pallets.Add(p);
-                //if (i % (n / 50) == 0)
-                //    Console.WriteLine($"{i/(n/100)}%     ");
                 Console.SetCursorPosition(0, 0);
+
             }
 
-            var all = pallets.OrderBy(p => p.ExpirationDate)
-                .ThenBy(p => p.weight)
-                .GroupBy(p => p.ExpirationDate);
+            var groups = Selector.GetOrganaizedPallets(pallets);
 
             var sb = new StringBuilder();
-            foreach (var date in all)
+            foreach (var group in groups)
             {
-                sb.Append($"\nGroup {date.Key}\n");
-                foreach (var pallet in date.OrderBy(p => p.weight))
+                sb.Append($"\nGroup {group.Date}\n");
+                foreach (var pallet in group.Pallets.OrderBy(p => p.Weight))
                 {
-                    sb.Append($"pal | [{pallet.id}] {date.Key}, weight: {pallet.weight}, boxes: {pallet.GetBoxes().Count}\n");
+                    sb.Append($"pal | [{pallet.Id}] {group.Date}, weight: {pallet.Weight}, boxes: {pallet.GetBoxes().Count}\n");
                     foreach (var dt in pallet.ExpirationDates)
                     {
                         sb.Append($"\t{dt}\n");
                     }
                 }
             }
-            sb.Append($"\n\n\n");
+            var most_longest_pallets = Selector.GetLongestStored(pallets);
 
-            var long_stored_p = pallets
-                .OrderByDescending(p => p.ExpirationDates.Max())
-                .Take(3)
-                .OrderBy(p => p.volume);
-            foreach (var pallet in long_stored_p)
+            foreach (var pallet in most_longest_pallets)
             {
-                sb.Append($"pal | [{pallet.id}] {pallet.ExpirationDates.Max()}, weight: {pallet.weight}, boxes: {pallet.volume}\n");
-                foreach (var dt in pallet.ExpirationDates)
-                {
-                    sb.Append($"\t{dt}\n");
-                }
+                sb.AppendLine($"[{pallet.Id}] {pallet.ExpirationDates.Max()}, volume: {pallet.Volume}");
             }
+
             Console.WriteLine(sb);
         }
     }
---
+++ ConsoleApp1/Classes.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public struct PalletDateGroup
+    {
+        public DateOnly Date;
+        public List<Pallet> Pallets;
+    }
+    public class Selector
+    {
+        public static IEnumerable<Pallet> GetLongestStored(IEnumerable<Pallet> pallets, int count = 3)
+        {
+            return pallets
+                .OrderByDescending(p => p.ExpirationDates.Max())
+                .Take(count)
+                .OrderBy(p => p.Volume);
+        }
+        public static IEnumerable<PalletDateGroup> GetOrganaizedPallets(IEnumerable<Pallet> pallets)
+        {
+            return pallets
+                .OrderBy(p => p.ExpirationDate)
+                .ThenBy(p => p.Weight)
+                .GroupBy(p => p.ExpirationDate)
+                .Select(group => new PalletDateGroup { Date = group.Key, Pallets = group.ToList() });
+        }
+    }
+    public class Box
+    {
+        public Box(
+            Guid id,
+            int width, int length, int heigth,
+            int weight,
+            DateOnly? manufactDate,
+            DateOnly? expirationDate)
+        {
+            this.Id = id;
+            this.Width = width;
+            this.Length = length;
+            this.Heigth = heigth;
+            this.Weight = weight;
+            this._manufactDate = manufactDate;
+            this._expirationDate = expirationDate;
+        }
+
+        public Box(
+            int width, int length, int heigth,
+            int weight,
+            DateOnly? manufactDate,
+            DateOnly? expirationDate)
+            : this(Guid.NewGuid(), width, length, heigth, weight, manufactDate, expirationDate)
+        { }
+        public Box() { }
+
+        private DateOnly? _manufactDate;
+        private DateOnly? _expirationDate;
+
+        public void SetExpirationDate(int year, int month, int day)
+            => _expirationDate = new DateOnly(year, month, day);
+
+        public void SetManufactionDate(int year, int month, int day)
+            => _manufactDate = new DateOnly(year, month, day);
+
+
+        public DateOnly ExpirationDate
+        {
+            get => _expirationDate ??
+                   _manufactDate?.AddDays(100) ??
+                   DateOnly.MinValue;
+        }
+
+        public Guid Id { get; }
+        public int Width { get; }
+        public int Length { get; }
+        public int Heigth { get; }
+        public int Weight { get; }
+        public int Volume { get => Width * Length * Heigth; }
+    }
+
+    public class Pallet
+    {
+        public Pallet(Guid id, int width, int length, int heigth)
+        {
+            this.Id = id;
+            this.Width = width;
+            this.Length = length;
+            this.P_heigth = heigth;
+
+            _boxes = new List<Box>();
+        }
+
+        public Pallet(int width, int length, int heigth)
+            : this(Guid.NewGuid(), width, length, heigth) { }
+
+        private List<Box> _boxes { get; set; }
+        public int P_heigth { get; }
+        public int P_volume
+        {
+            get => Width * Length * P_heigth;
+        }
+        public int Boxes_height
+        {
+            get => _boxes.Aggregate(0, (heigth, box) => heigth + box.Heigth);
+        }
+        public int Boxes_weight
+        {
+            get => _boxes.Aggregate(0, (weight, box) => weight + box.Weight);
+        }
+        public int Boxes_volume
+        {
+            get => _boxes.Aggregate(0, (volume, box) => volume + box.Volume);
+        }
+        const int P_weight = 30;
+
+        public DateOnly ExpirationDate
+        {
+            get => _boxes.Count > 0 ?
+                _boxes.Min(box => box.ExpirationDate) :
+                DateOnly.MinValue;
+        }
+        public List<DateOnly> ExpirationDates
+        {
+            get => _boxes
+                .Select(b => b.ExpirationDate)
+                .ToList();
+        }
+
+        public Guid Id { get; }
+        public int Width { get; }
+        public int Length { get; }
+        public int Heigth
+        {
+            get => P_heigth + Boxes_height;
+        }
+        public int Weight
+        {
+            get => P_weight + Boxes_weight;
+        }
+        public int Volume
+        {
+            get => P_volume + Boxes_volume;
+        }
+
+        public List<Box> GetBoxes()
+        {
+            return _boxes;
+        }
+        public bool AddBox(Box box)
+        {
+            var can_hold = (box.Width <= Width && box.Length <= Length) ||
+                           (box.Width <= Length && box.Width <= Length);
+
+            if (can_hold)
+            {
+                _boxes.Add(box);
+            }
+
+            return can_hold;
+        }
+    }
+}
